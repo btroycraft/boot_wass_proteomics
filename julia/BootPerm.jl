fe032ee88a261e725df5ed71576fffd673fb1bfa
Benjamin Roycraft <roycraft.benjamin@gmail.com>
2020-09-21 13:21:33 -0700

    update

--- julia/BootPerm.jl
+++ julia/BootPerm.jl
@@ -1,5 +1,6 @@
 using StatsBase: sample!
 using Random: shuffle!
+using Statistics: var
 
 split_groups = function(group_lab_vec)
   group_enum = collect(enumerate(group_lab_vec))
@@ -15,33 +16,27 @@ split_groups = function(group_lab_vec)
   return out_list, out_vec
 end
 
-boot_reps_cor = function(boot_reps, group_list, group_vec, data_mat, range)
+boot_reps = function(boot_reps, group_list)
 
-  out = Matrix{Int}(undef, length(group_vec), boot_reps)
+  out = Matrix{Int}(undef, sum([length(group) for group in group_list]), boot_reps)
 
-  vec_X = Vector{Float64}(undef, length(group_vec))
-  vec_samp = Vector{Int}(undef, length(group_vec))
+  vec_samp_list = [Vector{Int}(undef, length(group)) for group in group_list]
 
   for ind_boot in 1:boot_reps
     for ind_group in 1:length(group_list)
 
-      group = group_list[ind_group]
-      length_samp = length(group)
+      @inbounds group = group_list[ind_group]
+      @inbounds vec_samp = vec_samp_list[ind_group]
 
       @label LOOP_START
+
       sample!(group, vec_samp;
         replace = true)
 
-      for ind_range in 1:length(range)
-        for ind_samp in 1:length(group)
-          vec_X[ind_samp] = data_mat[group_vec[vec_samp[group[ind_samp]]], range[ind_range]]
-        end
-
-        all(vec_X[ind] == vec_X[1] for ind in 2:length(group)) && @goto LOOP_START
-      end
+      all(vec_samp[ind] == vec_samp[1] for ind in 2:length(group)) && @goto LOOP_START
 
       for ind_samp in 1:length(group)
-        out[group[ind_samp], ind_boot] = vec_samp[ind_samp]
+        @inbounds out[group[ind_samp], ind_boot] = vec_samp[ind_samp]
       end
     end
   end
@@ -49,35 +44,169 @@ boot_reps_cor = function(boot_reps, group_list, group_vec, data_mat, range)
   return out
 end
 
+boot_reps_cor_! = @inline function(group, group_vec, data_mat, range, vec_x = Vector{Float64}(undef, length(group)), vec_samp = Vector{Int}(undef, length(group)))
+
+  @label LOOP_START
+  sample!(group, vec_samp;
+    replace = true)
+
+  for ind_range in 1:length(range)
+    for ind_samp in 1:length(group)
+      @inbounds vec_x[ind_samp] = data_mat[group_vec[vec_samp[ind_samp]], range[ind_range]]
+    end
+
+    iszero(var(vec_x)) && @goto LOOP_START
+  end
+end
+
 perm_cor = function(perm_reps, group_list, group_vec, data_mat, range)
 
   out = Matrix{Int}(undef, length(group_vec), perm_reps)
 
-  vec_X = Vector{Float64}(undef, length(group_vec))
+  vec_x_list = [Vector{Float64}(undef, length(group)) for group in group_list]
   vec_samp = collect(1:length(group_vec))
 
   for ind_perm in 1:perm_reps
 
-    @label LOOP_START
-    shuffle!(vec_samp)
-
-    for ind_group in 1:length(group_list)
-
-      group = group_list[ind_group]
-
-      for ind_range in 1:length(range)
-        for ind_samp in 1:length(group)
-          vec_X[ind_samp] = data_mat[group_vec[vec_samp[group[ind_samp]]], range[ind_range]]
-        end
-
-        all(vec_X[ind] == vec_X[1] for ind in 2:length(group)) && @goto LOOP_START
-      end
-    end
+    perm_cor_!(group_list, group_vec, data_mat, range, vec_x_list, vec_samp)
 
     for ind_samp in 1:length(group_vec)
-      out[ind_samp, ind_perm] = vec_samp[ind_samp]
+      @inbounds out[ind_samp, ind_perm] = vec_samp[ind_samp]
     end
   end
 
   return out
+end
+
+perm_cor_! = @inline function(group_list, group_vec, data_mat, range, vec_x_list, vec_samp)
+
+  @label LOOP_START
+  shuffle!(vec_samp)
+
+  for ind_group in 1:length(group_list)
+
+    @inbounds group = group_list[ind_group]
+    @inbounds vec_x = vec_x_list[ind_group]
+
+    for ind_range in 1:length(range)
+      for ind_samp in 1:length(group)
+        @inbounds vec_x[ind_samp] = data_mat[group_vec[vec_samp[group[ind_samp]]], range[ind_range]]
+      end
+
+      iszero(var(vec_x)) && @goto LOOP_START
+    end
+  end
+end
+
+split_train = function(train_reps, train_size, test_size, group_list, group_vec, data_mat, range)
+
+  out_mat = Matrix{Int}(undef, train_size*length(group_list) + test_size, train_reps)
+  out_train = [((ind-1) * train_size+1):((ind-1)*train_size + train_size) for ind in 1:length(group_list)]
+  out_test = (length(group_list)*train_size + 1):(length(group_list)*train_size + test_size)
+  out_lab = Vector{Int}(undef, train_reps)
+
+  vec_samp_list = [collect(group) for group in group_list]
+  vec_samp_train_list = [Vector{Int}(undef, train_size) for _ in 1:length(group_list)]
+  vec_samp_test = Vector{Int}(undef, test_size)
+
+  vec_x_train_list = [Vector{Float64}(undef, train_size) for _ in 1:length(group_list)]
+  vec_x_test = Vector{Float64}(undef, test_size)
+
+  for ind_train in 1:train_reps
+
+    out_lab[ind_train] = split_train_!(group_list, group_vec, train_size, test_size, data_mat, range, vec_samp_list, vec_samp_train_list, vec_samp_test, vec_x_train_list, vec_x_test)
+
+    for ind_samp in 1:test_size
+      out_mat[out_test[ind_samp], ind_train] = vec_samp_test[ind_samp]
+    end
+
+    for ind_group in 1:length(group_list)
+      train = out_train[ind_group]
+      vec_samp_train = vec_samp_train_list[ind_group]
+
+      for ind_samp in 1:train_size
+        out_mat[train[ind_samp], ind_train] = vec_samp_train[ind_samp]
+      end
+    end
+  end
+
+  return out_train, out_test, out_lab, out_mat
+end
+
+split_train_! = @inline function(group_list, group_vec, train_size, test_size, data_mat, range, vec_samp_list, vec_samp_train_list, vec_samp_test, vec_x_train_list, vec_x_test)
+
+  @label LOOP_START
+
+  for vec_samp in vec_samp_list
+    shuffle!(vec_samp)
+  end
+
+  lab_test = rand(1:length(group_list))
+  vec_samp = vec_samp_list[lab_test]
+
+  for ind_samp in 1:test_size
+    vec_samp_test[ind_samp] = vec_samp[train_size + ind_samp]
+  end
+
+  for ind_range in 1:length(range)
+    for ind_samp in 1:test_size
+      vec_x_test[ind_samp] = data_mat[group_vec[vec_samp_test[ind_samp]], range[ind_range]]
+    end
+
+    iszero(var(vec_x_test)) && @goto LOOP_START
+  end
+
+  for ind_group in 1:length(group_list)
+
+    vec_samp = vec_samp_list[ind_group]
+    vec_samp_train = vec_samp_train_list[ind_group]
+    vec_x_train = vec_x_train_list[ind_group]
+
+    for ind_samp in 1:train_size
+      vec_samp_train[ind_samp] = vec_samp[ind_samp]
+    end
+
+    for ind_range in 1:length(range)
+      for ind_samp in 1:train_size
+        vec_x_train[ind_samp] = data_mat[group_vec[vec_samp_train[ind_samp]], range[ind_range]]
+      end
+
+      iszero(var(vec_x_train)) && @goto LOOP_START
+    end
+  end
+
+  return lab_test
+end
+
+boot_reps_train = function(boot_reps, train_list, test)
+
+  out_mat = Matrix{Int}(undef, sum([length(train) for train in train_list]) + length(test), boot_reps)
+
+  vec_samp_train_list = [Vector{Int}(undef, length(train)) for train in train_list]
+  vec_samp_test = Vector{Int}(undef, length(test))
+
+  for ind_boot in 1:boot_reps
+
+    sample!(test, vec_samp_test;
+      replace = true)
+
+    for ind_samp in 1:length(test)
+      out_mat[test[ind_samp], ind_boot] = vec_samp_test[ind_samp]
+    end
+
+    for ind_group in 1:length(train_list)
+
+      train = train_list[ind_group]
+      vec_samp_train = vec_samp_train_list[ind_group]
+
+      sample!(train, vec_samp_train;
+        replace = true)
+
+      for ind_samp in 1:length(test)
+        out_mat[train[ind_samp], ind_boot] = vec_samp_train[ind_samp]
+      end
+    end
+  end
+
+  return out_mat
 end
--- julia/WassBootCor.jl
+++ julia/WassBootCor.jl
@@ -59,6 +59,9 @@ wbc_pw = @inline function(ind1, ind2, group_list, group_vec, data_mat, range, bo
       end
 
       vec_cor[ind_boot] = trans == true ? atanh(cor(vec_x1, vec_x2)) : cor(vec_x1, vec_x2)
+      if isnan(vec_cor[ind_boot])
+        @show(vec_x1, vec_x2)
+      end
     end
 
     sort!(vec_cor)
@@ -127,11 +130,11 @@ wbc_perm_close = function(group_list, group_vec, data_mat, range, boot_mat, perm
   vec_calc = Vector{Float64}(undef, length(group_list))
 
   return function(sub)
-    return wbc_perm(sub, group_list, group_vec, data_mat, range, boot_mat, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc, group_vec_perm; wp, trans)
+    return wbc_perm(sub, group_list, group_vec, data_mat, range, boot_mat, perm_mat, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc, group_vec_perm; wp, trans)
   end
 end
 
-wbc_perm = @inline function(sub, group_list, group_vec, data_mat, range, boot_mat, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc, group_vec_perm; wp, trans)
+wbc_perm = @inline function(sub, group_list, group_vec, data_mat, range, boot_mat, perm_mat, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc, group_vec_perm; wp, trans)
 
   sum_orig = wbc_sum_subset(sub, group_list, group_vec, data_mat, range, boot_mat, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc; wp, trans)
 
@@ -163,4 +166,109 @@ wbc_sum_subset = @inline function(sub, group_list, group_vec, data_mat, range, b
   end
 
   return out / binomial(length(sub), 2)
+end
+
+cor_sum_subset = @inline function(sub, group_list, group_vec, data_mat, range, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc; lp, trans)
+
+  out = 0.
+  for ind1 in 1:length(sub)
+    for ind2 in (ind1+1):length(sub)
+      val = cor_pw(sub[ind1], sub[ind2], group_list, group_vec, data_mat, range, vec_x1_list, vec_x2_list, vec_cor, vec_cor_mat, vec_calc; lp, trans)
+      out += val
+    end
+  end
+
+  return out / binomial(length(sub), 2)
+end
+
+wbc_cv = function(sub_mat, train_list, test, group_vec_mat, data_mat, range, boot_mat; wp, trans)
+
+  group_vec_perm = Vector{Int}(undef, size(group_vec_mat, 1))
+  succ = Vector{Int}(undef, size(sub_mat, 2))
+
+  vec_x1_test = Vector{Float64}(undef, length(test))
+  vec_x2_test = copy(vec_x1_test)
+
+  vec_x1_train = [Vector{Float64}(undef, length(train)) for train in train_list]
+  vec_x2_train = deepcopy(vec_x1_train)
+
+  cor_vec_test = Vector{Float64}(undef, size(boot_mat, 2))
+  cor_vec_train = copy(cor_vec_test)
+
+  wass = Vector{Float64}(undef, length(train_list))
+  vote = Vector{Int}(undef, binomial(size(sub_mat, 1), 2))
+
+  succ_list = map(1:size(group_vec_mat, 2)) do ind_rep
+
+    succ = zeros(Int, size(sub_mat, 2))
+
+    for ind in 1:size(group_vec_mat, 1)
+      group_vec_perm[ind] = group_vec_mat[ind, ind_rep]
+    end
+
+    for ind_sub in 1:size(sub_mat, 2)
+        for ind in 1:size(sub_mat, 1)
+          sub[ind] = sub_mat[ind, ind_sub]
+        end
+
+        succ[ind_sub] = lab_vec[ind_rep] == wbc_cv_(sub, train_list, test, group_vec_perm, data_mat, range, boot_mat, vec_x1_test, vec_x2_test, vec_x1_list_train, vec_x2_list_train, cor_vec_test, cor_vec_train, wass, vote; wp, trans)
+    end
+
+    return succ
+  end
+
+  return map(1:size(sub_mat, 2)) do ind_sub
+    mean([succ[ind_sub] for succ in succ_list])
+  end
+end
+
+wbc_cv_ = function(sub, train_list, test, group_vec, data_mat, range, boot_mat, vec_x1_test, vec_x2_test, vec_x1_list_train, vec_x2_list_train, cor_vec_test, cor_vec_train, wass, vote; wp, trans)
+
+  ind_vote = 1
+  for ind1 in 1:length(sub)
+    for ind2 in (ind1+1):length(sub)
+
+      for ind_boot in 1:size(boot_mat, 2)
+        for ind_samp in 1:length(test)
+          vec_x1_test[ind_samp] = data_mat[group_vec[boot_mat[test[ind_samp], ind_boot]], range[ind_1]]
+          vec_x2_test[ind_samp] = data_mat[group_vec[boot_mat[test[ind_samp], ind_boot]], range[ind_2]]
+        end
+
+        cor_vec_test[ind_boot] = trans == true ? atanh(cor(vec_x1_test, vec_x2_test)) : cor(vec_x1_test, vec_x2_test)
+      end
+
+      sort!(core_vec_test)
+
+      for ind_group in 1:length(train_list)
+        train = train_list[ind_group]
+        vec_x1_train = vec_x1_train_list[ind_group]
+        vec_x2_train = vec_x2_train_list[ind_group]
+
+        for ind_samp in 1:length(train)
+          vec_x1_train[ind_samp] = data_mat[group_vec[boot_mat[train[ind_samp], ind_boot]], range[ind_1]]
+          vec_x2_train[ind_samp] = data_mat[group_vec[boot_mat[train[ind_samp], ind_boot]], range[ind_2]]
+        end
+
+        cor_vec_train[ind_boot] = trans == true ? atanh(cor(vec_x1_train, vec_x2_train)) : cor(vec_x1_train, vec_x2_train)
+
+        sort!(cor_vec_train)
+
+        total = 0.
+        for ind_boot in 1:size(boot_mat, 2)
+          if wp == 1
+            total += abs(cor_vec_train[ind_boot] - cor_vec_test[ind_boot])
+          elseif wp == 2
+            total += (cor_vec_train[ind_boot] - cor_vec_test[ind_boot])^2
+          end
+        end
+
+        wass[ind_group] = total
+      end
+
+      vote[ind_vote] = findmin(wass)
+      ind_vote += 1
+    end
+  end
+
+  return findmax(map(x -> sum(vote == x), 1:length(list_train)))
 end
